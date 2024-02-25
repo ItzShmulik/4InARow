eca1b4be73f091c878acae90b49170cbaf1dd747
shmulik55 <gshloman@gmail.com>
2024-02-25 20:33:53 +0200

    Started implementing add circle function

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ let currentPlayer = "p1";
 
 const buttons = document.getElementsByClassName("button");
 
+const rows = 6;
+const columns = 7;
+
+const arr = createArray(rows, columns);
+
 for (let index = 0; index < buttons.length; index++) {
     const button = buttons[index];
-    button.addEventListener("click", switchPlayer);
+    button.addEventListener("click", function(){
+        addCircle(parseInt(button.id));
+        switchPlayer();
+    });
 }
 
 function switchPlayer(){
@@ -15,4 +23,30 @@ function switchPlayer(){
         currentPlayer = "p1";
         document.body.style.backgroundColor = "#b32e45";
     }
+}
+
+function addCircle(id){
+    
+    let index = arr.length - 1;
+
+    console.log(arr[index][id]);
+
+    console.log("Played at: ");
+    console.log(index, id);
+
+    if(currentPlayer == "p1"){
+        arr[index][id] = 1;
+    }else{
+        arr[index][id] = 2;
+    }
+}
+
+function createArray(rows, columns){
+    let array = new Array(rows);
+
+    for (let index = 0; index < rows; index++) {
+        array[index] = new Array(columns);
+    }
+
+    return array;
 }
